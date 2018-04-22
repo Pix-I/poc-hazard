89985da8fa67979aa2d3f779c0b179c72e303d25
Jolan Stordeur <Pix@pix-labs.com>
2018-04-22 22:46:42 +0200

    Initial commit

---
+++ src/main/java/be/pix/labs/pochazard/PocHazardApplication.java
@@ -0,0 +1,12 @@
+package be.pix.labs.pochazard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocHazardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocHazardApplication.class, args);
+	}
+}
---
+++ src/test/java/be/pix/labs/pochazard/PocHazardApplicationTests.java
@@ -0,0 +1,16 @@
+package be.pix.labs.pochazard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocHazardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
